aaf7afc91f57f2f43158ce86e135088e045c22b8
NideverJoe <nidever.joe@gmail.com>
2019-07-16 22:18:42 -0500

    all working except for modal display

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,7 @@
+var path = require("path");
 
-var friendsArray=require(path.join(__dirname, './app/data/friends'));
+
+var friendsArray=require('../data/friends');
 
 module.exports = function(app){
 
@@ -9,5 +11,46 @@ app.get("/api/friends", function(req, res){
 
 // need to fix post. Not correct yet.
 app.post("/api/friends", function(req, res){
-    res.json(path.join(__dirname, "app/public/survey.html"));
-})}
+		// Capture the user input object
+		var userInput = req.body;
+		// console.log('userInput = ' + JSON.stringify(userInput));
+
+		var userResponses = userInput.scores;
+		// console.log('userResponses = ' + userResponses);
+
+		// Compute best friend match
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 9001; // Make the initial value big for comparison
+
+		// Examine all existing friends in the list
+		for (var i = 0; i < friendsArray.length; i++) {
+			console.log('friend = ' + JSON.stringify(friendsArray[i]));
+
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friendsArray[i].scores[j] - userResponses[j]);
+			}
+			// console.log('diff = ' + diff);
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				console.log('Closest match found = ' + diff);
+				console.log('Friend name = ' + friendsArray[i].name);
+				console.log('Friend image = ' + friendsArray[i].photo);
+
+				totalDifference = diff;
+				matchName = friendsArray[i].name;
+				matchImage = friendsArray[i].photo;
+				// document.getElementById("#match-name").textContent(matchName);
+				// document.getElementById("#match-img").setAttribute("src", matchImage);
+	  
+			}
+		}
+
+		// Send appropriate response
+        res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+    });
+
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,15 @@
 /////////////////////////
 //ROUTES//
 /////////////////////////
+var path = require("path");
+
+module.exports = function(app){
+
+
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
 })
 app.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
 })
+}
